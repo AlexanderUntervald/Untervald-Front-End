fd45b66c67303e59e5d11b6b6d0b8c69073a6ed0
Ivanfrankevych <32160039+Ivanfrankevych@users.noreply.github.com>
2020-11-16 06:47:15 +0200

    d

--- js/script.js
+++ js/script.js
@@ -16,46 +16,39 @@
 //let arr = Summ(2345);
 //console.log(arr);
 
- // HomeWork 2 - non
+// HomeWork 2 - non
 
- //function Average(a) {
-  //   let sum = 0;
- //    for (i = 0; i < a.lenght; i++){
-    //     sum += a[i];  
- //    }
-  //   let avr = sum / a.lenght; 
- //    return avr;
- //}
- //let arr = Average([10, 15, 22, 34]);
- //console.log(arr);
- 
-    
-  //function MyArray(a) {
-   //let sum  = 0;
-   //let sum1 = 0;
-   //let arr1 =[];
-   //for (let i = 0; i <a.length; i++ ){
-      // if (a[i] % 2 == 0){
-     //      arr.push(a[i]);
-       //}
- //  }   
-     //return arr1; 
- //}
- 
- //b =  MyArray([4, 7, 20, 15, 17, 22]);
- //console.log(sum1 - sum2);
- 
-//document.getElementById("change_color").addEventListener//("click",
-  //function(color) {
-   ////document.getElementsByClassName("second").style.backg//roundColor = "blue";
-  //}
-//);
-//a.addEventList.toggle('click', function () {
-    
+//function Average(a) {
+//   let sum = 0;
+//    for (i = 0; i < a.lenght; i++){
+//     sum += a[i];  
+//    }
+//   let avr = sum / a.lenght; 
+//    return avr;
 //}
+//let arr = Average([10, 15, 22, 34]);
+//console.log(arr);
 
 
+function MyArray(a) {
+    let sum = 0;
+    let sum1 = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] % 2 == 0) {
+            sum += a[i];
+        } else {
+            sum1 += a[i];
+        }
+    }
+    return sum - sum1;
+}
 
+b = MyArray([4, 7, 20, 15, 17, 22]);
+console.log(b);
+//console.log(sum1 - sum2);
 
-
-
+document.getElementById("change_color").addEventListener("click",
+    function () {
+        document.getElementsByClassName("second")[0].classList.add('blue');
+    }
+);
